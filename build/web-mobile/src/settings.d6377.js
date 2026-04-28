9b49246404dadf704812559fa4bfa58a41da178f
cupper <ncupper@gmail.com>
2026-04-28 14:39:15 +0300

    builds for pages

---
+++ build/web-mobile/src/settings.d6377.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/0.startup.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"52d36",main:"476c0"}};
